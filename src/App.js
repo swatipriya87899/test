3141d298f3dcd5801d510a2fbe66bde754d1901d
Swati Priya <swatipriya87899@gmail.com>
2024-07-05 16:22:31 +0530

    just for testing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Register from './Register'
 import Login from './Login'
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
  
   return (
     <div>
       {!isLoggedIn ? <Register setIsLoggedIn={setIsLoggedIn}/>: <Login/>}
+      <h1>Just for testing</h1>
+      <h1>For testing branch</h1>
     </div>
   )
 }
